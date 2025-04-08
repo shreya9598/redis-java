e53b15134469475c9a7508914a87c4c35318df7d
shreya <shreya@techcrumb.com>
2025-04-08 18:59:58 +0530

    resp command printing

--- app/src/main/java/com/opredis/ClientHandler.java
+++ app/src/main/java/com/opredis/ClientHandler.java
@@ -13,17 +13,64 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         try (
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+            InputStream input = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream()
         ) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("Received from client: " + line);
-                writer.write("Echo: " + line + "\n");
-                writer.flush();
+            while (true) {
+                StringBuilder rawCommand = new StringBuilder();
+                int c = input.read();
+
+                if (c == -1) break;
+
+                rawCommand.append((char) c);
+
+                // First byte should be '*'
+                if (c == '*') {
+                    String line = readLine(input);
+                    rawCommand.append(line).append("\r\n");
+
+                    int argsCount = Integer.parseInt(line);
+
+                    for (int i = 0; i < argsCount; i++) {
+                        int typeChar = input.read(); // should be $
+                        rawCommand.append((char) typeChar);
+
+                        String lenLine = readLine(input);
+                        rawCommand.append(lenLine).append("\r\n");
+
+                        int strLen = Integer.parseInt(lenLine);
+                        byte[] strData = input.readNBytes(strLen);
+                        rawCommand.append(new String(strData));
+
+                        // read trailing \r\n
+                        input.read(); // \r
+                        input.read(); // \n
+                        rawCommand.append("\r\n");
+                    }
+
+                    // Now print the full raw command received
+                    System.out.println("Full RESP command:\n" + rawCommand);
+
+                    // Respond with OK just to test
+                    output.write("+OK\r\n".getBytes());
+                    output.flush();
+                }
             }
         } catch (IOException e) {
             System.out.println("Client disconnected.");
         }
+    }
+
+    private String readLine(InputStream input) throws IOException {
+        ByteArrayOutputStream lineBuffer = new ByteArrayOutputStream();
+        int b;
+        while ((b = input.read()) != -1) {
+            if (b == '\r') {
+                input.read(); // skip \n
+                break;
+            }
+            lineBuffer.write(b);
+        }
+        return lineBuffer.toString();
     }
 }
